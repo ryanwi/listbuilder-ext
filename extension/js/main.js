1dac9d7881a2dc2abf6ccecf6f7b625bc1b1fede
Ryan Williams <rwilliamz@gmail.com>
2014-03-07 13:49:54 -0800

    Several updates to load the user’s lists on ready, and then wire up the button to add the selected users to the selected list.

--- extension/js/main.js
+++ extension/js/main.js
@@ -1,23 +1,23 @@
-var tweetSelector = "ol.stream-items>li[data-item-type='tweet']";
+(function( $ ) {
 
-if (typeof jQuery === 'undefined') {
-  console.error("no jquery");
-} else {
+  var tweetSelector = "ol.stream-items>li[data-item-type='tweet']";
+  var $selectLists = $("<select id='#twlistbuilder_lists'><option value='-1'> - Select a List - </option></select>");
+  var $btnAddMembers = $("<button type='button' class='btn'>Include selected users in list</button>").on("click", addToList);
+  var $btnToggleAll = $("<button type='button' class='btn'>Select All</button>").on("click", toggleAll);
+
   $(document).ready(function () {
+    console.log("ready");
 
     // popup action area to process selections
-    $selectAll = $("<button>Select all</button>").on("click", selectAll);
-    $deselectAll = $("<button>DeselectAll all</button>").on("click", deselectAll);
-    $lists = $("<select><option> - Select a List - </option></select>");
-
     $("div#timeline")
     .prepend(
       $("<div class='twlistbuilder-actions'>")
       .append("<h1>Twitter List Builder</h1>")
-      .append($selectAll)
-      .append($deselectAll)
-      .append($lists)
+      .append($selectLists)
+      .append($btnAddMembers)
+      .append($btnToggleAll)
       );
+    loadLists();
 
     // wire up selection, treat single selection as multiple selection:
     // http://stackoverflow.com/questions/4396042/implement-multiple-selects-with-jquery-ui-selectable
@@ -36,12 +36,65 @@ if (typeof jQuery === 'undefined') {
       }
     });
   });
-}
 
-function selectAll() {
-  $(tweetSelector).addClass('ui-selected');
-}
+  var selectAllOn = false;
+  function toggleAll() {
+    if (selectAllOn) {
+      $(tweetSelector).removeClass('ui-selected');
+      selectAllOn = false;
+    } else {
+      $(tweetSelector).addClass('ui-selected');
+      selectAllOn = true;
+    }
+    $btnToggleAll.text(selectAllOn ? "Clear All" : "Select All");
+  }
 
-function deselectAll() {
-  $(tweetSelector).removeClass('ui-selected');
-}
+  function addToList(e) {
+    // get selected list
+    var selectedListId = $selectLists.val();
+    if (selectedListId == "-1") {
+      alert("Please select a list.");
+      return;
+    }
+
+    // collect selected users
+    var user_ids = [];
+    var selected = $("div.tweet.ui-selectee.ui-selected");
+    selected.each(function (index, element) {
+      user_ids.push($(element).data("user-id"));
+    });
+
+    console.log(user_ids);
+    if (user_ids.length == 0) {
+      alert("Please select at least one tweet.");
+      return;
+    }
+
+    // /i/user_id/lists/list_id/members
+    $auth_token = $("input[type='hidden'][name='authenticity_token']").first().val();
+    $.each(user_ids, function(index, user_id) {
+      $.post("/i/" + user_id + "/lists/" + selectedListId + "/members",
+        { authenticity_token: $auth_token },
+        function (data) {
+          console.log("Added to list");
+        });
+    });
+  }
+
+  function loadLists() {
+    // grab any userid
+    var userId = "783214"; // @twitter
+    var jqxhr = $.getJSON( "/i/" + userId + "/lists", function( data ) {
+
+      // populate select list from response
+      var $lists = $(data.html).filter("ul.list-membership-container").children("li");
+      $lists.each(function (index, element) {
+        var listId = $(element).data("list-id");
+        var listName = $(element).text();
+        $selectLists.append($("<option>", { value: listId, text: listName }));
+      });
+
+    });
+  }
+
+})( jQuery );
