00bb9da1ab4561deb66de9a58a0d9abc83188221
Ryan Williams <rwilliamz@gmail.com>
2014-03-06 22:37:01 -0800

    Implement selectable to easily select tweets and start building action area.

--- extension/js/main.js
+++ extension/js/main.js
@@ -1,20 +1,47 @@
+var tweetSelector = "ol.stream-items>li[data-item-type='tweet']";
+
 if (typeof jQuery === 'undefined') {
   console.error("no jquery");
 } else {
-  console.log("extension ready")
-  $(document).ready( function () {
-    console.log("jQuery ready");
+  $(document).ready(function () {
 
-    $("div.stream-container").on(
-      "click", "li.stream-item", function(e) {
-        e.preventDefault();
-        // alert( $( this ).text() );
-        $( this ).css( 'background-color', 'red' );
-    }).on(
-      "mouseout", "li.stream-item", function( e ) {
-        // console.log( "item mouseover" );
-        $( this ).css( 'background-color', 'red' );
+    // popup action area to process selections
+    $selectAll = $("<button>Select all</button>").on("click", selectAll);
+    $deselectAll = $("<button>DeselectAll all</button>").on("click", deselectAll);
+    $lists = $("<select><option> - Select a List - </option></select>");
+
+    $("div#timeline")
+    .prepend(
+      $("<div class='twlistbuilder-actions'>")
+      .append("<h1>Twitter List Builder</h1>")
+      .append($selectAll)
+      .append($deselectAll)
+      .append($lists)
+      );
+
+    // wire up selection, treat single selection as multiple selection:
+    // http://stackoverflow.com/questions/4396042/implement-multiple-selects-with-jquery-ui-selectable
+    $(tweetSelector).on("onmousedown", function(e) {
+      e.metaKey = true;
+    }).selectable({
+      selected: function( event, ui ) {
+        var screen_name = $(this).children("div.tweet").data("screen-name");
+
+        // show a checkmark when selected
+        // console.log( $(this).find("a.account-group>img.avatar") );
+        // $(this).find("a.account-group>img.avatar").css(
+        //   "background-image",
+        //   "url('chrome-extension://__MSG_@@extension_id__/images/checkmark-circled.png)"
+        // );
+      }
     });
-
   });
+}
+
+function selectAll() {
+  $(tweetSelector).addClass('ui-selected');
+}
+
+function deselectAll() {
+  $(tweetSelector).removeClass('ui-selected');
 }
